772c3ef00781aff8567e53eafecd0f91ea9c8a65
Gagan D <76513704+D-Gagan@users.noreply.github.com>
2024-02-17 18:58:20 +0530

    Create script.js- inside main

---
+++ script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const socket = io();
+  
+    const form = document.querySelector('form');
+    const input = document.querySelector('#m');
+    const messages = document.querySelector('#messages');
+  
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      if (input.value) {
+        socket.emit('chat message', input.value);
+        input.value = '';
+      }
+    });
+  
+    socket.on('chat message', (msg) => {
+      const li = document.createElement('li');
+      li.textContent = msg;
+      messages.appendChild(li);
+    });
+  });
+  
